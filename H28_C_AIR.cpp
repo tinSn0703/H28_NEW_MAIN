61846331caa4a0ceae55423044c9daa204a16e0b
tinSn0703 <condensa1952@gmail.com>
2016-07-10 16:45:12 +0900

    1.5.1
    変数名変えた

--- H28_C_AIR.cpp
+++ H28_C_AIR.cpp
@@ -10,18 +10,18 @@ class C_AIR
 	{	
 		struct S_AIR
 		{
-			E_LOGIC air0 :1;
-			E_LOGIC air1 :1;
-			E_LOGIC air2 :1;
-			E_LOGIC air3 :1;
-			E_LOGIC air4 :1;
-			E_LOGIC air5 :1;
-			E_LOGIC air6 :1;
-			E_LOGIC air7 :1;
+			E_LOGIC _air0 :1;
+			E_LOGIC _air1 :1;
+			E_LOGIC _air2 :1;
+			E_LOGIC _air3 :1;
+			E_LOGIC _air4 :1;
+			E_LOGIC _air5 :1;
+			E_LOGIC _air6 :1;
+			E_LOGIC _air7 :1;
 		};
 		
-		T_DATA data_byte :8;
-		S_AIR data_bit;
+		T_DATA _data_byte :8;
+		S_AIR _data_bit;
 	};
 	
 	U_AIR _mem_air_data;
@@ -36,35 +36,35 @@ class C_AIR
 	void Turn();
 	void Turn_num(T_NUM );
 	
-	T_DATA Ret()						{	return _mem_air_data.data_byte;								}
-	E_LOGIC Ret_num(T_NUM _arg_air_num)	{	return CHECK_BIT_TF(_mem_air_data.data_byte,_arg_air_num);	}
+	T_DATA Ret()						{	return _mem_air_data._data_byte;								}
+	E_LOGIC Ret_num(T_NUM _arg_air_num)	{	return CHECK_BIT_TF(_mem_air_data._data_byte,_arg_air_num);	}
 	
-	void Set_0(E_LOGIC _arg_air_nf_0)	{	_mem_air_data.data_bit.air0 = _arg_air_nf_0;	}
-	void Set_1(E_LOGIC _arg_air_nf_1)	{	_mem_air_data.data_bit.air1 = _arg_air_nf_1;	}
-	void Set_2(E_LOGIC _arg_air_nf_2)	{	_mem_air_data.data_bit.air2 = _arg_air_nf_2;	}
-	void Set_3(E_LOGIC _arg_air_nf_3)	{	_mem_air_data.data_bit.air3 = _arg_air_nf_3;	}
-	void Set_4(E_LOGIC _arg_air_nf_4)	{	_mem_air_data.data_bit.air4 = _arg_air_nf_4;	}
-	void Set_5(E_LOGIC _arg_air_nf_5)	{	_mem_air_data.data_bit.air5 = _arg_air_nf_5;	}
-	void Set_6(E_LOGIC _arg_air_nf_6)	{	_mem_air_data.data_bit.air6 = _arg_air_nf_6;	}
-	void Set_7(E_LOGIC _arg_air_nf_7)	{	_mem_air_data.data_bit.air7 = _arg_air_nf_7;	}
+	void Set_0(E_LOGIC _arg_air_nf_0)	{	_mem_air_data._data_bit._air0 = _arg_air_nf_0;	}
+	void Set_1(E_LOGIC _arg_air_nf_1)	{	_mem_air_data._data_bit._air1 = _arg_air_nf_1;	}
+	void Set_2(E_LOGIC _arg_air_nf_2)	{	_mem_air_data._data_bit._air2 = _arg_air_nf_2;	}
+	void Set_3(E_LOGIC _arg_air_nf_3)	{	_mem_air_data._data_bit._air3 = _arg_air_nf_3;	}
+	void Set_4(E_LOGIC _arg_air_nf_4)	{	_mem_air_data._data_bit._air4 = _arg_air_nf_4;	}
+	void Set_5(E_LOGIC _arg_air_nf_5)	{	_mem_air_data._data_bit._air5 = _arg_air_nf_5;	}
+	void Set_6(E_LOGIC _arg_air_nf_6)	{	_mem_air_data._data_bit._air6 = _arg_air_nf_6;	}
+	void Set_7(E_LOGIC _arg_air_nf_7)	{	_mem_air_data._data_bit._air7 = _arg_air_nf_7;	}
 
-	void Turn_0()	{	_mem_air_data.data_bit.air0 = TURN_TF(_mem_air_data.data_bit.air0);	}
-	void Turn_1()	{	_mem_air_data.data_bit.air1 = TURN_TF(_mem_air_data.data_bit.air1);	}
-	void Turn_2()	{	_mem_air_data.data_bit.air2 = TURN_TF(_mem_air_data.data_bit.air2);	}
-	void Turn_3()	{	_mem_air_data.data_bit.air3 = TURN_TF(_mem_air_data.data_bit.air3);	}
-	void Turn_4()	{	_mem_air_data.data_bit.air4 = TURN_TF(_mem_air_data.data_bit.air4);	}
-	void Turn_5()	{	_mem_air_data.data_bit.air5 = TURN_TF(_mem_air_data.data_bit.air5);	}
-	void Turn_6()	{	_mem_air_data.data_bit.air6 = TURN_TF(_mem_air_data.data_bit.air6);	}
-	void Turn_7()	{	_mem_air_data.data_bit.air7 = TURN_TF(_mem_air_data.data_bit.air7);	}
+	void Turn_0()	{	_mem_air_data._data_bit._air0 = TURN_TF(_mem_air_data._data_bit._air0);	}
+	void Turn_1()	{	_mem_air_data._data_bit._air1 = TURN_TF(_mem_air_data._data_bit._air1);	}
+	void Turn_2()	{	_mem_air_data._data_bit._air2 = TURN_TF(_mem_air_data._data_bit._air2);	}
+	void Turn_3()	{	_mem_air_data._data_bit._air3 = TURN_TF(_mem_air_data._data_bit._air3);	}
+	void Turn_4()	{	_mem_air_data._data_bit._air4 = TURN_TF(_mem_air_data._data_bit._air4);	}
+	void Turn_5()	{	_mem_air_data._data_bit._air5 = TURN_TF(_mem_air_data._data_bit._air5);	}
+	void Turn_6()	{	_mem_air_data._data_bit._air6 = TURN_TF(_mem_air_data._data_bit._air6);	}
+	void Turn_7()	{	_mem_air_data._data_bit._air7 = TURN_TF(_mem_air_data._data_bit._air7);	}
 		
-	E_LOGIC Ret_0()	{	return _mem_air_data.data_bit.air0;	}
-	E_LOGIC Ret_1()	{	return _mem_air_data.data_bit.air1;	}
-	E_LOGIC Ret_2()	{	return _mem_air_data.data_bit.air2;	}
-	E_LOGIC Ret_3()	{	return _mem_air_data.data_bit.air3;	}
-	E_LOGIC Ret_4()	{	return _mem_air_data.data_bit.air4;	}
-	E_LOGIC Ret_5()	{	return _mem_air_data.data_bit.air5;	}
-	E_LOGIC Ret_6()	{	return _mem_air_data.data_bit.air6;	}
-	E_LOGIC Ret_7()	{	return _mem_air_data.data_bit.air7;	}
+	E_LOGIC Ret_0()	{	return _mem_air_data._data_bit._air0;	}
+	E_LOGIC Ret_1()	{	return _mem_air_data._data_bit._air1;	}
+	E_LOGIC Ret_2()	{	return _mem_air_data._data_bit._air2;	}
+	E_LOGIC Ret_3()	{	return _mem_air_data._data_bit._air3;	}
+	E_LOGIC Ret_4()	{	return _mem_air_data._data_bit._air4;	}
+	E_LOGIC Ret_5()	{	return _mem_air_data._data_bit._air5;	}
+	E_LOGIC Ret_6()	{	return _mem_air_data._data_bit._air6;	}
+	E_LOGIC Ret_7()	{	return _mem_air_data._data_bit._air7;	}
 	
 	void Do(T_NUM ,E_LOGIC );
 	void Do(T_NUM ,T_NUM ,E_LOGIC ,E_LOGIC &);
@@ -74,28 +74,28 @@ class C_AIR
 
 inline C_AIR::C_AIR(T_DATA _arg_air_data = 0x00)
 {
-	_mem_air_data.data_byte = _arg_air_data;
+	_mem_air_data._data_byte = _arg_air_data;
 	
 	for (usint i = 0; i < 8; i++)	_mem_array_air_flag[i] = TRUE;
 }
 
 inline void C_AIR::Set(T_DATA _arg_air_data)
 {
-	_mem_air_data.data_byte = _arg_air_data;
+	_mem_air_data._data_byte = _arg_air_data;
 }
 
 inline void C_AIR::Set_num(T_NUM _arg_air_num,E_LOGIC _arg_air_nf)
 {
 	switch (_arg_air_nf)
 	{
-		case TRUE:	_mem_air_data.data_byte |=  (1 << _arg_air_num);	break;
-		case FALES:	_mem_air_data.data_byte &= ~(1 << _arg_air_num);	break;
+		case TRUE:	_mem_air_data._data_byte |=  (1 << _arg_air_num);	break;
+		case FALES:	_mem_air_data._data_byte &= ~(1 << _arg_air_num);	break;
 	}
 }
 
 inline void C_AIR::Turn()
 {
-	_mem_air_data.data_byte = ~(_mem_air_data.data_byte);
+	_mem_air_data._data_byte = ~(_mem_air_data._data_byte);
 }
 
 inline void C_AIR::Turn_num(T_NUM  _arg_air_num)
@@ -195,7 +195,7 @@ void C_AIR::Out(C_UART_T _arg_air_uart_t)
 {
 	_arg_air_uart_t.Set_bit9(FALES);
 	
-	_arg_air_uart_t.Out(_mem_air_data.data_byte);
+	_arg_air_uart_t.Out(_mem_air_data._data_byte);
 }
 
 #endif
--- H28_C_MD_MAIN.cpp
+++ H28_C_MD_MAIN.cpp
@@ -15,33 +15,33 @@ class C_MD_MAIN
 	
 	union U_MD_0
 	{
-		struct S_MD_0
+		struct S_MD_0 //順番変えるな
 		{
-			E_SIG md_0_sig_mode	  :2;
-			E_LOGIC md_0_nf_steps :1;
-			T_NUM md_0_without	  :2;
-			T_NUM md_0_num_data	  :1;
-			T_NUM md_0_num_md	  :2;
-			T_NUM md_0_num_mdc	  :1;
+			E_SIG _md_0_sig_mode	:2;
+			E_LOGIC _md_0_nf_steps	:1;
+			T_NUM _md_0_without		:2;
+			T_NUM _md_0_num_data	:1;
+			T_NUM _md_0_num_md		:2;
+			T_NUM _md_0_num_mdc		:1; 
 			
 		};
 		
-		S_MD_0 md_0_data_bit;
-		T_DATA md_0_data :9;
+		S_MD_0 _md_0_data_bit;
+		T_DATA _md_0_data :9;
 	};
 	
 	union U_MD_1
 	{
-		struct S_MD_1
+		struct S_MD_1 //順番変えるな
 		{
-			T_PWM md_1_pwm_value :5;
-			T_NUM md_1_num_data	 :1;
-			T_NUM md_1_num_md	 :2;
-			T_NUM md_1_num_mdc	 :1;
+			T_PWM _md_1_pwm_value	:5;
+			T_NUM _md_1_num_data	:1;
+			T_NUM _md_1_num_md		:2;
+			T_NUM _md_1_num_mdc		:1;
 		};
 		
-		S_MD_1 md_1_data_bit;
-		T_DATA md_1_data :9;
+		S_MD_1 _md_1_data_bit;
+		T_DATA _md_1_data :9;
 	};
 	
 	U_MD_0 _mem_md_main_data_0;
@@ -70,30 +70,30 @@ class C_MD_MAIN
 	void Set_steps(E_LOGIC );
 	void Set_pwm_revis(sint );
 	
-	T_DATA Ret_data_0()	{	return _mem_md_main_data_0.md_0_data;	}
-	T_DATA Ret_data_1()	{	return _mem_md_main_data_1.md_1_data;	}
+	T_DATA Ret_data_0()	{	return _mem_md_main_data_0._md_0_data;	}
+	T_DATA Ret_data_1()	{	return _mem_md_main_data_1._md_1_data;	}
 	
-	T_NUM Ret_num_mdc()	{	return _mem_md_main_data_0.md_0_data_bit.md_0_num_mdc;	}
-	T_NUM Ret_num_md()	{	return _mem_md_main_data_0.md_0_data_bit.md_0_num_md;	}
-	E_SIG Ret_sig()		{	return _mem_md_main_data_0.md_0_data_bit.md_0_sig_mode;	}
-	T_PWM Ret_pwm()		{	return _mem_md_main_data_1.md_1_data_bit.md_1_pwm_value;}
-	E_LOGIC Ret_steps()	{	return _mem_md_main_data_0.md_0_data_bit.md_0_nf_steps;	}
+	T_NUM Ret_num_mdc()	{	return _mem_md_main_data_0._md_0_data_bit._md_0_num_mdc;	}
+	T_NUM Ret_num_md()	{	return _mem_md_main_data_0._md_0_data_bit._md_0_num_md;	}
+	E_SIG Ret_sig()		{	return _mem_md_main_data_0._md_0_data_bit._md_0_sig_mode;	}
+	T_PWM Ret_pwm()		{	return _mem_md_main_data_1._md_1_data_bit._md_1_pwm_value;}
+	E_LOGIC Ret_steps()	{	return _mem_md_main_data_0._md_0_data_bit._md_0_nf_steps;	}
 };
 
 inline C_MD_MAIN::C_MD_MAIN(T_NUM _arg_md_main_num_mdc, T_NUM _arg_md_main_num_md ,E_LOGIC _arg_md_main_nf_turn = FALES ,sint _arg_md_main_pwm_revis = 0)
 {
-	_mem_md_main_data_0.md_0_data_bit.md_0_num_mdc = _arg_md_main_num_mdc;
-	_mem_md_main_data_1.md_1_data_bit.md_1_num_mdc = _arg_md_main_num_mdc;
+	_mem_md_main_data_0._md_0_data_bit._md_0_num_mdc = _arg_md_main_num_mdc;
+	_mem_md_main_data_1._md_1_data_bit._md_1_num_mdc = _arg_md_main_num_mdc;
 	
-	_mem_md_main_data_0.md_0_data_bit.md_0_num_md = _arg_md_main_num_md;
-	_mem_md_main_data_1.md_1_data_bit.md_1_num_md = _arg_md_main_num_md;
+	_mem_md_main_data_0._md_0_data_bit._md_0_num_md = _arg_md_main_num_md;
+	_mem_md_main_data_1._md_1_data_bit._md_1_num_md = _arg_md_main_num_md;
 	
-	_mem_md_main_data_0.md_0_data_bit.md_0_num_data = MD_DATA_NUM_0;
-	_mem_md_main_data_1.md_1_data_bit.md_1_num_data = MD_DATA_NUM_1;
+	_mem_md_main_data_0._md_0_data_bit._md_0_num_data = MD_DATA_NUM_0;
+	_mem_md_main_data_1._md_1_data_bit._md_1_num_data = MD_DATA_NUM_1;
 	
-	_mem_md_main_data_0.md_0_data_bit.md_0_without = 0;
+	_mem_md_main_data_0._md_0_data_bit._md_0_without = 0;
 	
-	_mem_md_main_data_0.md_0_data_bit.md_0_sig_mode = ES_STOP;
+	_mem_md_main_data_0._md_0_data_bit._md_0_sig_mode = ES_STOP;
 	
 	_mem_md_main_pwm_revis = _arg_md_main_pwm_revis;
 	
@@ -102,8 +102,8 @@ inline C_MD_MAIN::C_MD_MAIN(T_NUM _arg_md_main_num_mdc, T_NUM _arg_md_main_num_m
 
 inline void C_MD_MAIN::Chan_md(T_NUM  _arg_md_main_num_md)
 {
-	_mem_md_main_data_0.md_0_data_bit.md_0_num_md = _arg_md_main_num_md;
-	_mem_md_main_data_1.md_1_data_bit.md_1_num_md = _arg_md_main_num_md;
+	_mem_md_main_data_0._md_0_data_bit._md_0_num_md = _arg_md_main_num_md;
+	_mem_md_main_data_1._md_1_data_bit._md_1_num_md = _arg_md_main_num_md;
 }
 
 inline void C_MD_MAIN::Set_turn(E_LOGIC _arg_md_main_nf_turn)
@@ -113,19 +113,19 @@ inline void C_MD_MAIN::Set_turn(E_LOGIC _arg_md_main_nf_turn)
 
 inline void C_MD_MAIN::Set_pwm(T_PWM _arg_md_main_pwm_value)
 {
-	_mem_md_main_data_1.md_1_data_bit.md_1_pwm_value = _arg_md_main_pwm_value;
+	_mem_md_main_data_1._md_1_data_bit._md_1_pwm_value = _arg_md_main_pwm_value;
 }
 
 inline void C_MD_MAIN::Set_sig(E_SIG _arg_md_main_sig_mode)
 {
-	if (CHECK_MOVE(_arg_md_main_sig_mode) == FALES)	_mem_md_main_data_1.md_1_data_bit.md_1_pwm_value = 0;
+	if (CHECK_MOVE(_arg_md_main_sig_mode) == FALES)	_mem_md_main_data_1._md_1_data_bit._md_1_pwm_value = 0;
 	
-	_mem_md_main_data_0.md_0_data_bit.md_0_sig_mode = _arg_md_main_sig_mode;
+	_mem_md_main_data_0._md_0_data_bit._md_0_sig_mode = _arg_md_main_sig_mode;
 }
 
 inline void C_MD_MAIN::Set_steps(E_LOGIC _arg_md_main_nf_steps = TRUE)
 {
-	_mem_md_main_data_0.md_0_data_bit.md_0_nf_steps = _arg_md_main_nf_steps;
+	_mem_md_main_data_0._md_0_data_bit._md_0_nf_steps = _arg_md_main_nf_steps;
 }
 
 inline void C_MD_MAIN::Set_pwm_revis(sint _arg_md_main_pwm_revis)
@@ -142,14 +142,14 @@ inline void C_MD_MAIN::Set_data(E_SIG _arg_md_main_sig_mode, T_PWM _arg_md_main_
 
 void C_MD_MAIN::Out(C_UART_T &_arg_md_main_uart_t)
 {
-	if (_mem_md_main_data_1.md_1_data_bit.md_1_pwm_value != 0) _mem_md_main_data_1.md_1_data_bit.md_1_pwm_value += _mem_md_main_pwm_revis;
+	if (_mem_md_main_data_1._md_1_data_bit._md_1_pwm_value != 0) _mem_md_main_data_1._md_1_data_bit._md_1_pwm_value += _mem_md_main_pwm_revis;
 	
-	if (_mem_md_main_nf_turn == TRUE)	_mem_md_main_data_0.md_0_data_bit.md_0_sig_mode = TURN_SIG_ROTATE(_mem_md_main_data_0.md_0_data_bit.md_0_sig_mode);
+	if (_mem_md_main_nf_turn == TRUE)	_mem_md_main_data_0._md_0_data_bit._md_0_sig_mode = TURN_SIG_ROTATE(_mem_md_main_data_0._md_0_data_bit._md_0_sig_mode);
 	
 	_arg_md_main_uart_t.Set_bit9(TRUE);
 	
-	_arg_md_main_uart_t.Out(_mem_md_main_data_0.md_0_data);
-	_arg_md_main_uart_t.Out(_mem_md_main_data_1.md_1_data);
+	_arg_md_main_uart_t.Out(_mem_md_main_data_0._md_0_data);
+	_arg_md_main_uart_t.Out(_mem_md_main_data_1._md_1_data);
 	
 	_delay_us(250);
 }
